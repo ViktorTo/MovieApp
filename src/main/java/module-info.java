90877951b41f0c1b055ad726531ec45cbeddd68e
JonathanAstrom <astrom.jonathan@gmail.com>
2022-03-23 15:55:17 +0100

    la till java.naming i module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.coolfilm.movieapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.hibernate.orm.core;
-
+    requires java.naming;
 
     opens com.coolfilm.movieapp to javafx.fxml;
     exports com.coolfilm.movieapp;
