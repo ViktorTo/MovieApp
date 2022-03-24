66e0fce012c59b2b537ceb856274a3161547681b
JonathanAstrom <astrom.jonathan@gmail.com>
2022-03-24 13:57:41 +0100

    ingen scrollsidebar

--- src/main/java/com/coolfilm/movieapp/HelloApplication.java
+++ src/main/java/com/coolfilm/movieapp/HelloApplication.java
@@ -11,9 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("demo-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 905, 576);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
-        stage.setTitle("Hello!");
+        stage.setTitle("CoolFilm AB");
         stage.setScene(scene);
         stage.show();
     }
